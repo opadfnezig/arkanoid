adcc2fd7177100750ec0e346184780a6f6d274f5
ROK10 <rok10wot@ukr.net>
2020-04-05 23:28:50 +0300

    Ball was created and can moving

---
+++ arkanoid/src/arkanoid/Ball.java
@@ -0,0 +1,47 @@
+package arkanoid;
+
+import acm.graphics.*;
+import acm.program.*;
+import java.awt.*; 
+import java.awt.event.*; 
+public class Ball {
+	GOval ball;
+	public static int ball_speed = 5;
+	public static int ball_radius = 20;
+	private static int width;
+	private static int height;
+
+	public Ball(int x, int y, int ball_radius) {
+		ball = new GOval(x, y, ball_radius, ball_radius);
+	}
+
+	public GOval getBall() {
+		return ball;
+	}
+
+	public void ballMoving() {
+		while (ball.getX() < height) {
+			//moveBall();
+			//checkForCollision();
+			//pause(DELAY);
+		}
+	}
+	public void mouseClicked(MouseEvent e) { 
+		ball.move(ball_speed, -ball_speed); 
+	}
+
+	public void move(int ball_speed) {
+		ball.move(ball_speed, -ball_speed);
+		
+	}
+	public void right(int windowWidth) {
+		if (ball.getX() <= windowWidth - ball.getWidth())
+			ball.move(10, 0);
+	}
+
+	public void left() {
+		if (ball.getX() >= 0)
+			ball.move(-10, 0);
+	}
+
+}
---
+++ arkanoid/src/arkanoid/Brick.java
@@ -0,0 +1,33 @@
+package arkanoid;
+
+import acm.graphics.GPoint;
+import acm.graphics.GRect;
+
+public class Brick
+{
+	private static int width;
+	private static int height;
+	private GRect rect;
+	public Brick(GPoint centerCoords) 
+	{
+		rect = new GRect(centerCoords.getX(),centerCoords.getY(),width,height); 
+	}
+	
+	public void setVisible(boolean value) { rect.setVisible(value); }	
+	public boolean isVisible() { return rect.isVisible(); }
+	
+	public GRect getGRect() { return rect; } 
+	
+	public static int getWidth() { return width; }
+	public static int getHeight() { return height; }
+	public static void setWidth(int width_) { width = width_; }
+	public static void setHeight(int height_) { height = height_; }
+	
+	public boolean ifPointInBrick(int x,int y)
+	{
+		if(rect.contains(x,y))
+			return true;
+		return false;
+	}
+	
+}
