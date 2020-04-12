f3e956a4d74ddd46a88baa6a44f022d2ba600e59
ThorIgor <44617152+ThorIgor@users.noreply.github.com>
2020-04-12 19:02:59 +0300

    main class

--- src/arkanoid/Arkanoid.java
+++ src/arkanoid/Arkanoid.java
@@ -9,106 +9,40 @@ import acm.program.GraphicsProgram;
 
 public class Arkanoid extends GraphicsProgram
 {
-	public static int width;
-	public static int height;
-	private static int brickCount; 
-	private static boolean ballStart = false;
-	private static int xSpacing;
-	private static int ySpacing;
-	private static int spacing;
+	private boolean end;
 	
-	private static int brickLineLengh = 10;
-	private static Brick bricks[];
+	private Ball ball;
+	private Board board;
+	private Menu menu;
 	
-	private static Board board;
-	private static Ball ball;
 	public void run()
 	{
-		levelSetup();
-		addMouseListeners(); 
-		render();
-	}
-	
-	private void render()
-	{
-		while(true)
+		setup();
+		while(end)
 		{
-			if (ballStart)
-			ball.move(Ball.ball_speed); 
-			pause(16);
-			
+			draw();
+			input();
+			logic();
 		}
 	}
-
-	public void init()
+	
+	public void setup()
 	{
-		//listeners
-		//inputListener = new UserInput();
-		//addMouseListener(inputListener);
-		//addKeyListener(inputListener);
-		addMouseListeners();
-		addKeyListeners();
-		//window
-		width = 1000;
-		height = 500;
-		this.setSize(width,height);
-		//bricks
-		Brick.setWidth(50);
-		Brick.setHeight(20);
-		brickCount = 40;
-		xSpacing = Brick.getWidth() + spacing;
-		ySpacing = Brick.getHeight() + spacing;
-		spacing = 10;
-		bricks = new Brick[brickCount];
-		board = new Board(0,height-30,Board.boardWidth,Board.boardHeight);
-		ball = new Ball(Board.boardWidth/2 - Ball.ball_radius/2,height-30-Board.boardHeight, Ball.ball_radius);
-		add(board.getBoard());
-		add(ball.getBall());
+		
 	}
 	
-	private void levelSetup()
+	public void draw()
 	{
-		for(int  y = 1,bricksPlaced = 0;(y < brickCount/brickLineLengh+1)&&(brickCount - bricksPlaced != 0);++y)
-			for(int x = 0;(x<brickLineLengh+1)&&(brickCount - bricksPlaced != 0);++x, ++bricksPlaced)
-			{
-				bricks[bricksPlaced] = new Brick(new GPoint(x*xSpacing,y*ySpacing));
-				add(bricks[bricksPlaced].getGRect());
-			}
-				
-	}
-	//метод провірки на колізію. Повертає Brick в випадку зіткнення, якщо зіткнення немає повертає null
-	public Brick checkForCollision(int x,int y)
-	{
-		for(int i = 0;i<bricks.length;++i)
-			if(bricks[i].ifPointInBrick(x,y)&&(bricks[i].isVisible()))
-				return bricks[i];
-		return null;
-	}
-	public void mouseClicked(MouseEvent arg0)
-	{
-		ballStart = true;
-	//	for(int i = 0;i<bricks.length;++i)
-	//		if(bricks[i].ifPointInBrick(arg0.getX(), arg0.getY()))
-	//			bricks[i].setVisible(false);
+		
 	}
 	
-	public void keyPressed(KeyEvent arg0)
+	public void input()
 	{
-		switch(arg0.getKeyCode())
-		{
-		case KeyEvent.VK_RIGHT:
-			board.right(width);
-			if(ballStart == false) 
-				ball.right(width);
-			break;
-		case KeyEvent.VK_LEFT:
-			board.left();
-			if(ballStart == false)
-				ball.left();
-			break;
-		}
-        
+		
 	}
 	
-	
+	public void logic()
+	{
+		
+	}
 }
