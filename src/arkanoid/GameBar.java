f8c3c2de764ec784eaa8959390d9c5815fd0b8b2
opadfnezig <opadfnezig@HP>
2020-04-19 15:24:32 +0300

    added GameBar

--- src/arkanoid/GameBar.java
+++ src/arkanoid/GameBar.java
@@ -1,19 +1,34 @@
 package arkanoid;
 
+import java.awt.Color;
+
 import acm.graphics.GCompound;
 import acm.graphics.GImage;
 import acm.graphics.GLabel;
 import acm.graphics.GLine;
+import acm.graphics.GRect;
 
 public class GameBar extends GCompound
 {
-	int ballCount;
-	
+	int ballCount = 0;
+	GLabel label;
+	public void setBallCount(int ballCount) 
+	{
+		this.ballCount = ballCount;
+		label.setLabel("x " + ballCount);
+		
+	}
+
 	public GameBar(int wWidth)
 	{
+		label = new GLabel("x " + ballCount,25, 15);
+		GRect rect = new GRect(0,0,wWidth,20);
+		rect.setFillColor(Color.white);
+		rect.setFilled(true);
+		add(rect);
 		add(new GLine(0,0,wWidth,0));
-		add(new GImage("*/images/ball.png"),0,0);
-		add(new GLabel("x " + ballCount,20, 0));
+		add(new GImage("ball.png"),0,0);
+		add(label);
 	}
 	
 	
