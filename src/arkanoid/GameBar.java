d7099976557aa2b27fffce68475f55f088283a4c
opadfnezig <opadfnezig@HP>
2020-04-19 19:13:38 +0300

    edited gameBar

--- src/arkanoid/GameBar.java
+++ src/arkanoid/GameBar.java
@@ -12,18 +12,18 @@ public class GameBar extends GCompound
 {
 	int ballCount = 0;
 	GLabel label;
-	public void setBallCount(int ballCount) 
+	public void setBallCount(int ballCount, int lvlNumber) 
 	{
 		this.ballCount = ballCount;
 		label.setLabel("x " + ballCount);
-		
+		add(new GLabel("lvl " +  lvlNumber),50,15);
 	}
-
+	
 	public GameBar(int wWidth)
 	{
 		label = new GLabel("x " + ballCount,25, 15);
 		GRect rect = new GRect(0,0,wWidth,20);
-		rect.setFillColor(Color.white);
+		rect.setFillColor(new Color(0,48,109));
 		rect.setFilled(true);
 		add(rect);
 		add(new GLine(0,0,wWidth,0));
